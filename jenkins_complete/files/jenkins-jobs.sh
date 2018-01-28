eedad32fd412ef363b6734e321ccbd34715f72f2
Hanmanth Reddy Patukuri -X (hpatukur - INFOSYS LIMITED at Cisco) <hpatukur@partnet.cisco.com>
2018-01-28 22:46:22 +0530

    modified final changes

--- jenkins_complete/files/jenkins-jobs.sh
+++ jenkins_complete/files/jenkins-jobs.sh
@@ -6,22 +6,22 @@ jenkins_home=/var/lib/jenkins
 	for job in $*
 	do
 
-		if [ !-d "$jenkins_home"/jobs/"$1" ]
+		if [ ! -d "${jenkins_home}"/jobs/"${1}" ]
 		then
 
-			java -jar $jenkins_home/jenkins-cli.jar  -s http://localhost:8080/  create-job $1  <  /tmp/$1_config.xml
+			java -jar ${jenkins_home}/jenkins-cli.jar  -s http://localhost:8080/  create-job ${1}  <  /tmp/${1}_config.xml
 
 		else
 
-			test = `diff -qr $jenkins_home/jobs/$1/config.xml  $1_config.xml`
+			test = `diff -qr ${jenkins_home}/jobs/${1}/config.xml  ${1}_config.xml`
 
-			if [ $test != "" ]
+			if [ "${test}" != "" ]
 			then
-				java -jar $jenkins_home/jenkins-cli.jar  -s http://localhost:8080/  update-job $1  <  /tmp/$1_config.xml
+				java -jar ${jenkins_home}/jenkins-cli.jar  -s http://localhost:8080/  update-job ${1}  <  /tmp/${1}_config.xml
 			if
 
 			echo "Sample job is already present.
 
-		fi  
+		fi
 
 	done
