23b67722f771fd9d730a6a62400f059d1962490c
Hanmanth Reddy Patukuri -X (hpatukur - INFOSYS LIMITED at Cisco) <hpatukur@partnet.cisco.com>
2018-01-28 20:17:14 +0530

    modified jenkins-jobs.sh

--- jenkins_complete/files/jenkins-jobs.sh
+++ jenkins_complete/files/jenkins-jobs.sh
@@ -6,16 +6,16 @@ jenkins_home=/var/lib/jenkins
 	for job in $*
 	do
 
-		if [ !-d "$jenkins_home"/"$1" ]
+		if [ !-d "$jenkins_home"/jobs/"$1" ]
 		then
 
 			java -jar $jenkins_home/jenkins-cli.jar  -s http://localhost:8080/  create-job $1  <  /tmp/$1_config.xml
 
 		else
 
-			test= `diff -qr $jenkins_home/$1  $1_config.xml`
+			test = `diff -qr $jenkins_home/jobs/$1/config.xml  $1_config.xml`
 
-			if [ $test == "diifer" ]
+			if [ $test == "" ]
 			then
 				java -jar $jenkins_home/jenkins-cli.jar  -s http://localhost:8080/  update-job $1  <  /tmp/$1_config.xml
 			if
